f72af4d30b2001f96f6ad0d3f2808812cd40b2d0
Kavi <thekavisharma26@gmail.com>
2026-06-09 17:47:36 +0530

    Added Subarray k sum and count

--- Day08/LengthOfLongestSubarray.cpp
+++ Day08/LengthOfLongestSubarray.cpp
@@ -1,31 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int longestSubarray(vector<int> &nums){
+int longestSubarray(vector<int> &nums , int k){
     unordered_map<int,int> mpp;
+    mpp[0]  = -1;
     int sum = 0 ;
-    int maxLen = -1;
+    int maxLen = 0;
     for(int i = 0 ; i < nums.size() ; i++){
         sum += nums[i];
 
-        if(sum == 0){
-            maxLen = i+1;
+        if(mpp.find(sum-k)!= mpp.end()){
+            maxLen = max(maxLen, i  - mpp[sum - k]);
         }
 
         if(mpp.find(sum) == mpp.end()){
             mpp[sum] = i;
-        }
-        else{
-            maxLen = max(maxLen , i - mpp[sum]);
         }
 
 
     }
     return maxLen;
 }
+
+int countSubarrays(vector<int> &nums , int k){
+    unordered_map<int,int> mpp;
+    int sum = 0 ;
+    int count = 0;
+    for(int i = 0 ; i < nums.size() ; i++){
+        sum += nums[i];
+
+        if(sum == k){
+            count++;
+        }
+
+        if(mpp.find(sum - k) != mpp.end()){
+            count+=mpp[sum - k];
+            
+        }
+        mpp[sum]++;
+
+
+    }
+    return count;
+}
+
 int main(){
-    vector<int> nums = {9, -3, 3, -1, 6, -5};
-    cout<<longestSubarray(nums);
+    vector<int> nums = {-3, 2 , 1};
+    cout<<longestSubarray(nums , 6);
     
 
 }
