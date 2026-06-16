dedae15c079ce91a3ea1bb58869da4ca458afbda
Kavi <thekavisharma26@gmail.com>
2026-06-16 11:37:49 +0530

    Job Sequencing

--- Day15/JobSequencing.cpp
+++ Day15/JobSequencing.cpp
@@ -1,75 +1,85 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-class Job {
+class Job{
 public:    
-    int jobId;
-    int completionDeadline;
-    int earningValue;
-    Job(int jobId, int completionDeadline, int earningValue){
-        this->jobId = jobId;
-        this->completionDeadline = completionDeadline;
-        this->earningValue = earningValue;
-    }
-};
-
-class Solution {
-public:
-    static bool compareByProfit(Job a, Job b) {
-        return (a.earningValue > b.earningValue); 
+    int id;
+    int deadLine;
+    int profit;
+    Job(int id , int deadLine , int profit){
+        this->id = id;
+        this->deadLine = deadLine;
+        this->profit = profit;
     }
     
-    pair<int, int> scheduleJobs(vector<Job*>& jobList) {
-        sort(jobList.begin(), jobList.end(), [](Job* x, Job* y) {
-            return x->earningValue > y->earningValue;
-        });
-        
-        int maxDeadline = 0;
-        for(Job* task : jobList) {
-            maxDeadline = max(maxDeadline, task->completionDeadline);
+};
+class Solution {
+public:
+    static bool comparison(Job* a,Job* b) {
+        return a->profit > b->profit; 
+    }
+    void jobSort(Job* arr[] , int n){
+        sort(arr , arr + n , comparison);
+    }
+    void print(Job* arr[] , int n){
+        for(int i = 0 ; i < n ; i++ ){
+            cout<<arr[i]->id<<" "<<arr[i]->deadLine<<" "<<arr[i]->profit<<endl;
         }
-        
-        vector<int> timeSlots(maxDeadline + 1, -1);
-        int completedJobCount = 0;
-        int totalEarning = 0;
-        
-        for(Job* currentJob : jobList) {
-            for(int slotTime = currentJob->completionDeadline; slotTime > 0; slotTime--) {
-                if(timeSlots[slotTime] == -1) {
-                    timeSlots[slotTime] = currentJob->jobId;
-                    completedJobCount++;
-                    totalEarning += currentJob->earningValue;
-                    break;
+    }
+    int maxDeadline(Job* arr[] , int n){
+        int maxDead = INT_MIN;
+        for(int i = 0 ; i < n ; i++){
+            maxDead = max(maxDead , arr[i]->deadLine);
+        }
+        return maxDead;
+    }
+    vector<int> maxProfit(Job* arr[] , int n){
+        int maxi = maxDeadline(arr , n);
+        int nums[maxi+1];
+        for(int i = 0 ; i < n ; i++){
+            nums[i] = -1;
+        }
+        int maxProfitEarned = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[arr[i]->deadLine] == -1){
+                maxProfitEarned+=arr[i]->profit;
+                nums[arr[i]->deadLine] = arr[i]->id;
+            }
+            else{
+                int indexValue = arr[i]->deadLine;
+                while(indexValue > 0){
+                    if(nums[indexValue] == -1){
+                        maxProfitEarned+=arr[i]->profit;
+                        nums[indexValue] = arr[i]->id;
+                        break;
+                    }
+                    indexValue--;
                 }
             }
         }
-        
-        return make_pair(completedJobCount, totalEarning);
+        int count = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i]!=-1){
+                count++;
+            }
+        }
+        return {count , maxProfitEarned};
     }
-};
 
+};
 int main(){
-    int totalJobs = 4;
-    
-    Job* taskOne = new Job(1, 2, 20);
-    Job* taskTwo = new Job(2, 1, 10);
-    Job* taskThree = new Job(3, 2, 40);
-    Job* taskFour = new Job(4, 2, 30);
-    
-    vector<Job*> jobCollection;
-    jobCollection.push_back(taskOne);
-    jobCollection.push_back(taskTwo);
-    jobCollection.push_back(taskThree);
-    jobCollection.push_back(taskFour);
-    
-    Solution scheduler;
-    pair<int, int> result = scheduler.scheduleJobs(jobCollection);
-    
-    cout << "Jobs Completed: " << result.first << " | Total Profit: " << result.second << endl;
-    
-    for(Job* task : jobCollection) {
-        delete task;
+    int n = 5;
+    Job* job1 = new Job(1, 2, 100);
+    Job* job2 = new Job(2, 1, 19);
+    Job* job3 = new Job (3, 2, 27);
+    Job* job4 = new Job(4, 1, 25);
+    Job* job5 = new Job(5, 1, 15);
+    Job* arr[] = {job1 , job2 , job3 , job4 , job5};
+    Solution obj;
+    obj.jobSort(arr,n);
+    vector<int> ans = obj.maxProfit(arr , n);
+    for(int i : ans){
+        cout<<i<<" ";
     }
     
-    return 0;
+
 }
